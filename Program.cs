697e9b0eca8a960426cba844bc68294ddc10f546
Hopeman22 <juantaban@gmail.com>
2020-02-10 16:53:59 +0100

    han vuelto los bugs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace EnigmaXD
         [STAThread]
         static void Main()
         {
+            //pene
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
